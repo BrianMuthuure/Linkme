57c6dfd59dbe71049a67ec59ff5c5d679981fa8b
Brian Muthuure <brianmuthuure@gmail.com>
2026-03-16 10:47:06 +0300

    chore:update django yml file

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from decouple import config
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS', default='linkme.settings.dev'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
